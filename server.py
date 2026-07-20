44ab02024d57ea4db05f4091223d18061650a9f7
teya365 <mis2eon@gmail.com>
2026-07-20 10:59:43 +0900

    Update server.py

--- server.py
+++ server.py
@@ -1,20 +1,10 @@
 from flask import Flask, request, jsonify, send_from_directory
-import json
+import requests
 import os
 
 app = Flask(__name__)
 
-FILE = 'data.json'
-
-if os.path.exists(FILE):
-    with open(FILE, 'r', encoding='utf-8') as f:
-        data = json.load(f)
-else:
-    data = []
-
-def save():
-    with open(FILE, 'w', encoding='utf-8') as f:
-        json.dump(data, f, ensure_ascii=False)
+API_URL = "https://script.google.com/macros/s/AKfycbwfDEWx_lpPlJK3jJZdnrzcUaQi21oogv_Ytqs4cs8M5bv8Gbf77fdsqYkxzXDePTCfPg/exec"
 
 @app.route('/')
 def home():
@@ -22,22 +12,14 @@ def home():
 
 @app.route('/add', methods=['POST'])
 def add():
-    data.append(request.json)
-    save()
-    return 'ok'
-
-@app.route('/delete', methods=['POST'])
-def delete():
-    idx = request.json['index']
-    if 0 <= idx < len(data):
-        data.pop(idx)
-        save()
+    data = request.json
+    requests.post(API_URL, json=data)
     return 'ok'
 
 @app.route('/list')
 def list_data():
-    return jsonify(data)
+    res = requests.get(API_URL)
+    return jsonify(res.json())
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
